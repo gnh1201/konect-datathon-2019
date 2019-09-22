e22fd419d2c9a178023ebfbb493ec28b28d9c8ed
Namhyeon, Go <gnh1201@gmail.com>
2019-09-22 13:25:29 +0900

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -13,32 +13,38 @@ from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
 
 names = [
-    'is_critical',
+    'person_id',
+    'x_occurrence',
     'x_psychiatric',
     'xn_psychiatric',
-    'x_occurrence',
+    'is_psychiatric',
     'x_visit',
     'x_procedure',
     'avg_occu_duration',
     'min_occu_duration',
     'max_occu_duration',
-    'x_death',
+    'is_death',
     'x_age',
-    'x_sex',
+    'is_sex_male',
+    'is_sex_female',
     'x_mellitus',
     'x_hypertension',
     'x_alz',
     'x_critical',
+    'is_critical',
     'x_crit_duration',
     'x_crit0_psychiatric',
     'x_crit1_psychiatric',
-    'x_crit2_psychiatric'
+    'x_crit2_psychiatric',
+    'd0',
+    'd1',
+    'd2'
 ]
 
-df = pd.read_csv('results-20190922-043020-notitle.csv', header = None, names = names)
+df = pd.read_csv('results-20190922-131603-notitle.csv', header = None, names = names)
 
 Y = df['is_critical']
-X = df.drop(['is_critical'], axis=1) # df.iloc[x1:y1, x2:y2]
+X = df.drop(['person_id', 'is_critical', 'd0', 'd1', 'd2'], axis=1) # df.iloc[x1:y1, x2:y2]
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
 
 clf = LogisticRegression()
